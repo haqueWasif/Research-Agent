8e426ae0e41f1edf6b4dfd0bf7be68a68c444a33
HaqueWasif~ <haquewasiful2014@gmail.com>
2025-10-29 16:51:26 +0600

    update

--- .streamlit/package.sh
+++ .streamlit/package.sh
@@ -1,3 +1,7 @@
 apt-get update
-apt-get install -y pandoc texlive-xetex texlive-fonts-recommended texlive-fonts-extra texlive-latex-extra
-
+apt-get install -y \
+    pandoc \
+    texlive-xetex \
+    texlive-fonts-recommended \
+    texlive-fonts-extra \
+    texlive-latex-extra
