01676fc792d67433676ed8a3a4b42779d7d86267
HaqueWasif~ <haquewasiful2014@gmail.com>
2025-10-31 14:56:37 +0600

    updated

--- .streamlit/package.sh
+++ .streamlit/package.sh
@@ -2,8 +2,13 @@ apt-get update
 apt-get install -y \
     pandoc \
     texlive-xetex \
+    texlive-plain-generic \
     texlive-fonts-recommended \
     texlive-fonts-extra \
     texlive-latex-extra \
+    texlive-latex-recommended \
     fonts-libertinus \
-    texlive-lang-cjk
+    texlive-lang-cjk \
+    lmodern
+
+
